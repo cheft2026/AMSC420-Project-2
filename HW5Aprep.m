72cdea637d9de4bbaa3c640c0371e0d0b6a2bd96
cheft2026 <cheft2026@gmail.com>
2026-04-28 18:49:58 -0400

    Update and rename HW5A.m to HW5Aprep.m

--- HW5Aprep.m
+++ HW5Aprep.m
@@ -15,16 +15,16 @@ title("New HIV Infections in California")
 ylabel("Year")
 
 %% Initial conditions
-initial_conditions = [200000; 150000; 5000; 1000; 500];
+initial_conditions = [40049097.68; 1538750.378; 5000; 3000; 2000];
 
-%% Fixed parameters (no PrEP)
+%% Fixed parameters with PrEP
 pi_val = 376078.91;
 mu_val = 0.009043;
 sigma_val = 0.0885;
-tau_I = 0;
-tau_A = 0;
-epsilon = 0;
-eta = 0.5;
+tau_I = 0.2;
+tau_A = 0.25;
+epsilon = 0.99;
+eta = 0.7;
 q = 0.037;
 
 A = [pi_val, mu_val, sigma_val, tau_I, tau_A, epsilon, eta, q];
@@ -63,18 +63,45 @@ hold off
 %% Display fitted parameters
 beta_fit
 
-function I_out = model_wrapper(b, t, IC, A)
+function new_cases = model_wrapper(b, t, IC, A)
     beta  = b(1);
-    deltaI = b(2);
-    deltaA = b(3);
-    params = [A, beta, deltaI, deltaA];
+    delta_I = b(2);
+    delta_A = b(3);
+    params = [A, beta, delta_I, delta_A];
 
     [t_sol, sol] = ode45(@(t,y) disease_dynamics(t,y,params), [t(1), t(end)], IC);
 
-    I_out = interp1(t_sol, sol(:,3), t);
+    % Extract states
+    Sl = sol(:,1);
+    Sh = sol(:,2);
+    I  = sol(:,3);
+    A_state  = sol(:,4);   % avoid name conflict
+    N  = sum(sol,2);
+
+    eta = params(7);
+    epsilon = params(6);
+    beta = params(9);
+
+    incidence = beta*((eta*I + A_state)./N).*(Sl + (1-epsilon)*Sh);
+
+        % Return values at data time points
+    new_cases = interp1(t_sol, incidence, t, 'linear', 'extrap');
+
+    % new_cases = zeros(size(t));
+    % 
+    % for i = 2:length(t)
+    %     % indices for this year interval
+    %     idx = (t_sol >= t(i-1)) & (t_sol <= t(i));
+    % 
+    %     if sum(idx) > 1
+    %         new_cases(i) = trapz(t_sol(idx), incidence(idx));
+    %     else
+    %         new_cases(i) = 0;
+    %     end
+    % end
+    % % handle first point (no previous year)
+    % new_cases(1) = new_cases(2);
 end
-
-
 function ddt = disease_dynamics(t, states, params)
     Sl = states(1);
     Sh = states(2);
@@ -112,6 +139,6 @@ delta_A = beta_fit(:,3);
 R0 = (beta*eta*(tau_A+mu_val+delta_A)+sigma_val*beta)/((sigma_val+tau_I+mu_val+delta_I)*(tau_A+mu_val+delta_A))
 RV = ((beta*eta*(tau_A+mu_val+delta_A)+sigma_val*beta)*(q-epsilon*q))/((sigma_val+tau_I+mu_val+delta_I)*(tau_A+mu_val+delta_A))
 
-hit =(1-(1/R0))
+hit = (1/epsilon)*(1-(1/R0))
 
 ci = nlparci(beta_fit,resid,'jacobian', J)
